70a493fad582e240abee3f727e7535ef6bcb20d3
Miles <Miles@192.168.2.18>
2021-01-30 01:11:31 -0500

    First Commit-PartTime

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -12,13 +12,34 @@ package ca.sheridancollege.week1.softwarefundamentals;
 public class Student {
     
     private String name;
+    private String 
 
     /**
      * Get the value of name
      *
      * @return the value of name
      */
-    public String getName() {
+    
+    private String string = numHours;
+
+    /**
+     * Get the value of string
+     *
+     * @return the value of string
+     */
+    public String getString() {
+        return string;
+    }
+
+    /**
+     * Set the value of string
+     *
+     * @param string new value of string
+     */
+    public void setString(String string) {
+        this.string = string;
+    }
+public String getName() {
         return name;
     }
 
